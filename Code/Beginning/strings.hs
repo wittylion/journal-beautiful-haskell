c57ad03ff53b05f9ed1df4e0a277577a3bfe04ef
SergAtGitHub <Serg.Solomentsev@ya.ru>
2020-10-31 16:42:49 +0000

    Added code samples of using strings.

---
+++ Code/Beginning/strings.hs
@@ -0,0 +1,30 @@
+-- STRINGS --
+
+-- Multiline string:
+-- make string more readable
+-- use '\' character. Be carefule, it does
+-- not add new line by defualt.
+sql = "\
+\SELECT * \
+\FROM companies \
+\WHERE company = 'Witty Lion'\
+\"
+-- SELECT * FROM companies WHERE company = 'Witty Lion'
+
+-- New line is \n character
+pretty_sql = "\
+\SELECT * \n\
+\FROM companies \n\
+\WHERE company = 'Witty Lion'\n\
+\"
+
+-- Join strings with space.
+space_joined = unwords [
+    "SELECT *",
+    "FROM companies",
+    "WHERE company = 'Witty Lion'"]
+
+-- Join strings with new line character.
+line_joined = unlines [
+    "There is a house in New Orleans",
+    "They call the Rising Sun"]
